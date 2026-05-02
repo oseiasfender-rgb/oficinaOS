ac0861e23b61167824b2fa72616a3ccc48b83c89
oseiasfender-rgb <oseias.fender@gmail.com>
2026-05-02 18:16:52 -0300

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-const UI = {
-  show(id){
-    document.querySelectorAll('.panel')
-      .forEach(p => p.classList.remove('active'));
-
-    document.getElementById(id).classList.add('active');
-    Render.all();
-  }
-};
