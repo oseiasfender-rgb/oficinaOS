b03e1c67613e66ea5059496b4e5898e674674fdc
oseiasfender-rgb <oseias.fender@gmail.com>
2026-05-02 18:07:30 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,7 +48,28 @@ const Financeiro = {
 
 /* =========================
    AGENDA
-========================= */
+========================= const Agenda = {
+
+  next(){
+    Store.agenda.mes++;
+    if(Store.agenda.mes > 11){
+      Store.agenda.mes = 0;
+      Store.agenda.ano++;
+    }
+    Render.agenda();
+  },
+
+  prev(){
+    Store.agenda.mes--;
+    if(Store.agenda.mes < 0){
+      Store.agenda.mes = 11;
+      Store.agenda.ano--;
+    }
+    Render.agenda();
+  }
+
+};
+*/
 const Agenda = {
 
   next(){
