d58acc87266cc5e21bfbbeb32b7110eee9a30082
oseiasfender-rgb <oseias.fender@gmail.com>
2026-05-02 18:16:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -44,7 +44,16 @@ const UI = {
 
     Render.all();
   }
+};const UI = {
+  show(id){
+    document.querySelectorAll('.panel')
+      .forEach(p => p.classList.remove('active'));
+
+    document.getElementById(id).classList.add('active');
+    Render.all();
+  }
 };
+
 
 /* =========================
    FINANCEIRO
