adc51df12c50def665c8da4a788d2bf504a1f256
oseiasfender-rgb <oseias.fender@gmail.com>
2026-05-02 18:10:25 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,23 @@ Render.all();<script>
 /* =========================
    STORE GLOBAL (ESSENCIAL)
 ========================= */
+const Financeiro = {
+
+  add(){
+    const desc = document.getElementById('desc').value;
+    const valor = parseFloat(document.getElementById('valor').value);
+
+    if(!desc || isNaN(valor)) return;
+
+    Store.financeiro.push({desc, valor});
+
+    document.getElementById('desc').value = '';
+    document.getElementById('valor').value = '';
+
+    Render.financeiro();
+  }
+
+};
 const Store = {
   financeiro: [],
   agenda: {
